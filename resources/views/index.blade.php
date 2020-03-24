0b7adeee766fb598996bd89f81de1bd2067e8722
0kkun <shinji.okumoto.01@gmail.com>
2020-03-24 15:14:36 +0900

    Initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>Shinji's portfolio</title>
+  <link rel="stylesheet" href="/sass/index.scss">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<div class="header">
+<h1>Shinji's portfolio</h1>
+</div>
+
+<div class="container">
+  テスト
+  <div class="container__navigation">
+    <ul>
+      <li class="navi-link">profile</li>
+      <li class="navi-link">works</li>
+      <li class="navi-link">contact</li>
+    </ul>
+  </div>
+
+
+  <h1>Blog Posts</h1>
+  <ul>
+    <li><a href="">title</a></li>
+    <li><a href="">title</a></li>
+    <li><a href="">title</a></li>
+  </ul>
+</div>
+
+<div class="footer">
+  footer
+</div>
+
+</html>
