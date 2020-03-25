aa1cf45351f91259545da3edd371959450aef0b5
0kkun <shinji.okumoto.01@gmail.com>
2020-03-25 11:59:51 +0900

    front amount

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,35 +3,60 @@
 <head>
   <meta charset="utf-8">
   <title>Shinji's portfolio</title>
-  <link rel="stylesheet" href="/sass/index.scss">
+  <!-- <link rel="stylesheet" href="/sass/index.scss"> -->
+  <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 
-<div class="header">
-<h1>Shinji's portfolio</h1>
-</div>
+  <div class = "wrapper">
 
-<div class="container">
-  テスト
-  <div class="container__navigation">
-    <ul>
-      <li class="navi-link">profile</li>
-      <li class="navi-link">works</li>
-      <li class="navi-link">contact</li>
-    </ul>
+    <div class="header">
+      <div class="background-image">
+        <div class="title"> Shinji's portfolio </div>
+        <div class="job"> Web Enginieer </div>
+      </div>
+    </div>
+
+  <div class="container">
+    <div class="navis">
+      <ul class="navi-list">
+        <li class="fas fa-user-alt"><a href="#" class="navi-link">profile</a></li>
+        <li class="fas fa-tasks"><a href="#" class="navi-link">works</a></li>
+        <li class="fas fas fa-envelope"><a href="#" class="navi-link">contact</a></li>
+        <li class="fab fa-blogger-b"><a href="#" class="navi-link">blog</a></li>
+      </ul>
+    </div>
+
+  <div class="section profile">
+    <div class="fas fa-user-alt section-title">PROFILE</div>
+      <p class="text">
+        　初めまして。shinjiと申します。平成1年生まれ、30歳エンジニアのshinjiです。
+        これまで電子部品の会社でメカニカルエンジニア6年、経理1年、サプライチェーン1年を経験しました。
+        サプライチェーンでIoTのツールを使い業務の自動化・見える化を行い、ツール自体を作りたいと思うようになりました。
+        そして2019/12からTech::Expertで Ruby on rails を学習し2020/02に卒業しました。
+        今後は社会に役立ち、人の生活を豊かにできるアプリケーションを作れるエンジニアになりたいと思っています。
+      </p>
+
+  </div
+
+>
+  <div class="section contact">
+    <div class="section-title">CONTACT</div>
+      <div class="section-desc">何かありましたらTwitterのDMからご連絡ください。</div>
+      <a href="https://twitter.com/Shin_Spcl_Prg"> Twitter </a>
   </div>
 
 
-  <h1>Blog Posts</h1>
-  <ul>
-    <li><a href="">title</a></li>
-    <li><a href="">title</a></li>
-    <li><a href="">title</a></li>
-  </ul>
-</div>
+  <div class="footer">
+      <ul class="footer__navi-list">
+        <li class="footer__navi-link">profile</li>
+        <li class="footer__navi-link">works</li>
+        <li class="footer__navi-link">contact</li>
+      </ul>
+    <div class="copy-rihgt"> Copyright©shinji. All Rights Reserved.</div>
+  </div>
 
-<div class="footer">
-  footer
-</div>
 
+</div>
 </html>
