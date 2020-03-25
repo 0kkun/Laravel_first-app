05662a9c6a1a19638ef81424dd0e46d86481924d
0kkun <shinji.okumoto.01@gmail.com>
2020-03-25 17:48:35 +0900

    front update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -19,40 +19,48 @@
     </div>
 
   <div class="container">
+
     <div class="navis">
       <ul class="navi-list">
-        <li class="fas fa-user-alt"><a href="#" class="navi-link">profile</a></li>
-        <li class="fas fa-tasks"><a href="#" class="navi-link">works</a></li>
-        <li class="fas fas fa-envelope"><a href="#" class="navi-link">contact</a></li>
-        <li class="fab fa-blogger-b"><a href="#" class="navi-link">blog</a></li>
+        <li class="fas fa-user-alt"><a href="#" class="navi-link">PROFILE</a></li>
+        <li class="fas fa-tasks"><a href="#" class="navi-link">WORKS</a></li>
+        <li class="fas fas fa-envelope"><a href="#" class="navi-link">CONTACT</a></li>
+        <!-- <li class="fab fa-blogger-b"><a href="#" class="navi-link">blog</a></li> -->
       </ul>
     </div>
 
+
   <div class="section profile">
-    <div class="fas fa-user-alt section-title">PROFILE</div>
-      <p class="text">
-        　初めまして。shinjiと申します。平成1年生まれ、30歳エンジニアのshinjiです。
+    <div class="fas fa-user-alt section-title"> PROFILE</div>
+    <div class="profile-contents">
+      <div class="profile-image"></div>
+      <div class="text">
+        　初めまして。shinjiと申します。平成1年生まれ、30歳です。
         これまで電子部品の会社でメカニカルエンジニア6年、経理1年、サプライチェーン1年を経験しました。
-        サプライチェーンでIoTのツールを使い業務の自動化・見える化を行い、ツール自体を作りたいと思うようになりました。
-        そして2019/12からTech::Expertで Ruby on rails を学習し2020/02に卒業しました。
+        サプライチェーンでIoTのツールを使い業務の自動化・見える化を行いました。その経験を通じて、
+        私も人の役に立つツールを作りたいと思うようになり、webエンジニアになることを決意しました。
+        そして2019/12からTech::Expertで Ruby on rails を学習し2020/02に卒業、現在はDockerとLaravelを勉強中です。
         今後は社会に役立ち、人の生活を豊かにできるアプリケーションを作れるエンジニアになりたいと思っています。
-      </p>
+      </div>
+    </div>
+  </div>
 
-  </div
+  <div class="section works">
+    <div class="fas fa-tasks section-title"> WORKS</div>
+  </div>
 
->
   <div class="section contact">
-    <div class="section-title">CONTACT</div>
+    <div class="fas fas fa-envelope section-title"> CONTACT</div>
       <div class="section-desc">何かありましたらTwitterのDMからご連絡ください。</div>
-      <a href="https://twitter.com/Shin_Spcl_Prg"> Twitter </a>
+      <a href="https://twitter.com/Shin_Spcl_Prg" class="fab fa-twitter"> </a>
   </div>
 
 
   <div class="footer">
       <ul class="footer__navi-list">
-        <li class="footer__navi-link">profile</li>
-        <li class="footer__navi-link">works</li>
-        <li class="footer__navi-link">contact</li>
+        <li class="footer__navi-link"><a href="#" class="footer__link">profile</a></li>
+        <li class="footer__navi-link"><a href="#" class="footer__link">works</a></li>
+        <li class="footer__navi-link"><a href="#" class="footer__link">contact</a></li>
       </ul>
     <div class="copy-rihgt"> Copyright©shinji. All Rights Reserved.</div>
   </div>
